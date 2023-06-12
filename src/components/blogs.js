50dd9c10af1db6b3cb99021b1e90bb78dfb8ff30
Monu Dixit <monudixit0007@gmail.com>
2023-06-12 10:35:06 +0530

    file structure done

---
+++ src/components/blogs.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import BlogCard from './blogCard'
+
+const Blogs = () => {
+  return (
+    <>
+    blogs
+    <BlogCard/>
+    </>
+  )
+}
+
+export default Blogs
---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Blogs from './blogs'
+
+const Home = () => {
+  return (
+    <>
+    <Blogs/>
+    </>
+  )
+}
+
+export default Home
